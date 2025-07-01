e370b85056fa553734ad5fa20c83260d6d4051ab
Sabbir <sh.mihad.cse@gmail.com>
2025-07-02 00:11:29 +0600

    get posts using token

--- src/hooks/useAPi.js
+++ src/hooks/useAPi.js
@@ -7,48 +7,52 @@ const useApi = () => {
   const { authInfo, setAuthInfo } = useAuth();
   const refreshToken = authInfo?.refreshToken;
   useEffect(() => {
-    const requestInterceptor = api.interceptors.request.use(
-      (config) => {
-        config.headers.Authorization = `Bearer ${authInfo.accessToken}`;
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
-      }
-    );
+    let requestInterceptor;
+    let responseInterceptor;
+    if (authInfo?.accessToken) {
+       requestInterceptor = api.interceptors.request.use(
+        (config) => {
+          config.headers.Authorization = `Bearer ${authInfo.accessToken}`;
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
+        }
+      );
 
-    const responseInterceptor = api.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const originalRequest = error.config;
-        if (error.status === 401 && originalRequest._retry) {
-          originalRequest._retry = true;
-          if (refreshToken) {
-            try {
-              const response = await axios.post(
-                `${import.meta.env.VITE_SERVER_BASE_URL}/auth/refresh-token`,
-                { refreshToken }
-              );
-              setAuthInfo(response.data);
-              originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+       responseInterceptor = api.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+          const originalRequest = error.config;
+          if (error.status === 401 && originalRequest._retry) {
+            originalRequest._retry = true;
+            if (refreshToken) {
+              try {
+                const response = await axios.post(
+                  `${import.meta.env.VITE_SERVER_BASE_URL}/auth/refresh-token`,
+                  { refreshToken }
+                );
+                setAuthInfo(response.data);
+                originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
 
-              return axios(originalRequest)
-            } catch (error) {
-                Promise.reject(error)
+                return axios(originalRequest);
+              } catch (error) {
+                Promise.reject(error);
+              }
             }
           }
+          return Promise.reject(error);
         }
-        return Promise.reject(error);
-      }
-    );
-
-    return ()=>{
-        api.interceptors.request.eject(requestInterceptor)
-        api.interceptors.response.eject(responseInterceptor)
+      );
     }
-  }, [authInfo.accessToken, refreshToken, setAuthInfo]);
 
-  return {api}
+    return () => {
+      api.interceptors.request.eject(requestInterceptor);
+      api.interceptors.response.eject(responseInterceptor);
+    };
+  }, [authInfo?.accessToken, refreshToken, setAuthInfo]);
+
+  return { api };
 };
 
 export default useApi;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../api/api";
+import useApi from "../../hooks/useAPi";
 import useAuth from "../../hooks/useAuth";
 import Modal from "../../modal/Modal";
 import LoginForm from "../auth/LoginForm";
@@ -7,9 +8,12 @@ import Post from "./components/Post";
 
 export default function HomePage() {
   const [posts, setPosts] = useState([])
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(10)
   const [isShowModal, setIsShowModal] = useState(false)
   const { authInfo } = useAuth()
- console.log("isShowModal",isShowModal);
+  const { api: axiosAPi } = useApi()
+  console.log("isShowModal", isShowModal);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY >= (document.body.offsetHeight - 1000) && !authInfo?.user?._id) {
@@ -32,12 +36,33 @@ export default function HomePage() {
           setPosts(response.data)
         }
       } catch (error) {
-        console.log("error-",error.config._retry);
+        console.log("error-", error);
       }
     }
 
     fetchPosts()
   }, [])
+
+  useEffect(() => {
+    async function getPosts() {
+      console.log("executing....");
+      if (authInfo?.user._id) {
+        try {
+          const response = await axiosAPi.get(`/posts/?page=${page}&limit=${limit}`);
+          if (response.status === 200) {
+            setPosts(response?.data)
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+
+    }
+
+    getPosts()
+  }, [authInfo?.user._id, page, limit, axiosAPi])
+
   return (
     <div className="max-w-6xl mx-auto w-full py-10  ">
       <div>
@@ -46,8 +71,8 @@ export default function HomePage() {
         }
       </div>
       {
-        isShowModal && <Modal isShowClose={false} onClose={()=>setIsShowModal(false)}>
-         <LoginForm/>
+        isShowModal && <Modal isShowClose={false} onClose={() => setIsShowModal(false)}>
+          <LoginForm />
         </Modal>
       }
     </div>
